9cf298382806320ed0a5d42864bc3cf7830d4fb8
Niraj Tiwari <tniraj7@gmail.com>
2020-08-08 23:47:56 +0900

    create a neumorphic button

--- SimpliLearn/SimpliLearn/ContentView.swift
+++ SimpliLearn/SimpliLearn/ContentView.swift
@@ -2,7 +2,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            Text("Button")
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                .frame(width: 200, height: 60)
+                .background(
+                    ZStack {
+                        Color(#colorLiteral(red: 0.8007653372, green: 0.8816142425, blue: 0.9373870802, alpha: 1))
+                        
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .foregroundColor(.white)
+                            .blur(radius: 4)
+                            .offset(x: -8, y: -8)
+                        
+                        
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .fill(
+                                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .padding(4)
+                        .blur(radius: 2)
+                    }
+            )
+                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .shadow(color: Color(#colorLiteral(red: 0.738869863, green: 0.8113246438, blue: 0.900390625, alpha: 1)), radius: 20, x: 20, y: 20)
+                .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(#colorLiteral(red: 0.8796049263, green: 0.9333333333, blue: 0.9624625428, alpha: 1)))
+        .edgesIgnoringSafeArea(.all)
+        
     }
 }
 
