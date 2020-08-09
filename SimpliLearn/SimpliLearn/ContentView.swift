57595946e40ad3317921c26e55025af09f6f1631
Niraj Tiwari <tniraj7@gmail.com>
2020-08-09 23:09:22 +0900

    add tap gesture for button press and animation on button

--- SimpliLearn/SimpliLearn/ContentView.swift
+++ SimpliLearn/SimpliLearn/ContentView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tap = false
+    
     var body: some View {
         
         VStack {
@@ -24,10 +26,20 @@ struct ContentView: View {
                         .padding(4)
                         .blur(radius: 2)
                     }
-            )
+                )
                 .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
                 .shadow(color: Color(#colorLiteral(red: 0.738869863, green: 0.8113246438, blue: 0.900390625, alpha: 1)), radius: 20, x: 20, y: 20)
                 .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+                .scaleEffect(tap ? 1.2 : 1)
+                .gesture(
+                    LongPressGesture().onChanged { value in
+                        self.tap = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            self.tap = false
+                        }
+                    }
+                )
+                .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.8796049263, green: 0.9333333333, blue: 0.9624625428, alpha: 1)))
@@ -38,6 +50,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 11")
     }
 }
