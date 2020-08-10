0ac420d94df110aa1b21152532211cc492d97acf
Niraj Tiwari <tniraj7@gmail.com>
2020-08-10 23:51:14 +0900

    add rotation3D effect to change between sun and moon images

--- SimpliLearn/SimpliLearn/ContentView.swift
+++ SimpliLearn/SimpliLearn/ContentView.swift
@@ -43,7 +43,7 @@ struct RectangleButton: View {
                     RoundedRectangle(cornerRadius: 16, style: .continuous)
                         .fill(
                             LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                    )
+                        )
                         .padding(2)
                         .blur(radius: 2)
                 }
@@ -81,44 +81,49 @@ struct RectangleButton: View {
 }
 
 struct CircleButton: View {
-    
     @State var tap = false
     @State var press = false
     
     var body: some View {
-        VStack {
+        ZStack {
             Image(systemName: "sun.max")
                 .font(.system(size: 44, weight: .light))
+                .offset(x: press ? -90 : 0, y: press ? -90 : 0)
+                .rotation3DEffect(Angle(degrees: press ? 20 : 0), axis: (x: 10, y: -10, z: 0))
+            Image(systemName: "moon")
+            .font(.system(size: 44, weight: .light))
+            .offset(x: press ? 0 : 90, y: press ? 0 : 90)
+            .rotation3DEffect(Angle(degrees: press ? 0 : 20), axis: (x: -10, y: 10, z: 0))
         }
         .frame(width: 100, height: 100)
         .background(
             ZStack {
-                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 
                 Circle()
                     .stroke(Color.clear, lineWidth: 10)
-                    .shadow(color: Color(#colorLiteral(red: 0.7450980392, green: 0.8, blue: 0.8980392157, alpha: 1)), radius: 3, x: -5, y: -5)
+                    .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 3, x: -5, y: -5)
                 
                 Circle()
                     .stroke(Color.clear, lineWidth: 10)
-                    .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 3, x: 3, y: 3)
+                    .shadow(color: Color(press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 3, x: 3, y: 3)
             }
-            )
-            .clipShape(Circle())
-            .shadow(color: Color.white, radius: 20, x: -20, y: -20)
-            .shadow(color: Color(#colorLiteral(red: 0.7450980392, green: 0.8, blue: 0.8980392157, alpha: 1)), radius: 20, x: 20, y: 20)
-            .scaleEffect(tap ? 1.2 : 1)
-            .gesture(
-                LongPressGesture()
-                    .onChanged { value in
-                        self.tap = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                            self.tap = false
-                        }
+        )
+        .clipShape(Circle())
+        .shadow(color: Color(press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 20, x: -20, y: -20)
+        .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+        .scaleEffect(tap ? 1.2 : 1)
+        .gesture(
+            LongPressGesture()
+            .onChanged { value in
+                self.tap = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    self.tap = false
                 }
-                .onEnded { value in
-                    self.press.toggle()
-                }
-            )
+            }
+            .onEnded { value in
+                self.press.toggle()
+            }
+        )
     }
 }
