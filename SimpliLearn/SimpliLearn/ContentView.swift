071424c438c7e0d1f54ea1fb49753bda8579bf67
Niraj Tiwari <tniraj7@gmail.com>
2020-08-16 23:45:44 +0900

    add ring to the fingerprint scan progress in payment button

--- SimpliLearn/SimpliLearn/ContentView.swift
+++ SimpliLearn/SimpliLearn/ContentView.swift
@@ -167,6 +167,17 @@ struct PayButton: View {
             }
         )
         .clipShape(Circle())
+        .overlay(
+            Circle()
+                .trim(from: tap ? 0.001 : 1, to: 1)
+                .stroke(LinearGradient(gradient: Gradient(colors: [ Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                .frame(width: 88, height: 88)
+                .rotationEffect(Angle(degrees: 90))
+                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                .shadow(color: Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 5, x: 3, y: 3)
+                
+                .animation(.easeInOut)
+        )
         .shadow(color: Color(press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 20, x: -20, y: -20)
         .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
         .scaleEffect(tap ? 1.2 : 1)
